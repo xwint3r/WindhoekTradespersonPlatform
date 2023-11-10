f1e319bb736c5a063c49133aa39b96dd8005f1ad
xwint3r <133570721+xwint3r@users.noreply.github.com>
2023-11-10 11:26:44 +0200

    l

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,6 @@
 class Location < ApplicationRecord
     has_many :services
+    def self.ransackable_attributes(auth_object = nil)
+        ["created_at", "id", "name", "updated_at"]
+      end
 end
