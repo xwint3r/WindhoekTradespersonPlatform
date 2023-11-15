75e49f93dfe1b47e89c85b8f1fc5a5a4a9e319d1
xwint3r <133570721+xwint3r@users.noreply.github.com>
2023-11-15 23:35:04 +0200

    DMs working
    DMs now work
    Always run redis or it'll crash cause we set that as out default cache

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
     validates_uniqueness_of :name
     scope :public_rooms, -> { where(is_private: false) }
-    after_create_commit { broadcast_if_public }
+    after_create_commit { broadcast_if_public } #only broadcasting a channel/room if it's public
     has_many :messages
     has_many :participants, dependent: :destroy
   
@@ -10,7 +10,7 @@ class Room < ApplicationRecord
       broadcast_append_to 'rooms' unless is_private
     end
   
-    
+
     def self.create_private_room(users, room_name)
       single_room = Room.create(name: room_name, is_private: true)
       users.each do |user|
