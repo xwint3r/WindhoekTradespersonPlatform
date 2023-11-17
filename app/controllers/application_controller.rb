2d0d9c9cf74f5d9de006690a4156f69959bdf4d4
xwint3r <133570721+xwint3r@users.noreply.github.com>
2023-11-17 03:03:05 +0200

    Application Controller X Turbo p1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     include Pagy::Backend
+    before_action :turbo_frame_request_varient
+
 
     protected
   
@@ -9,4 +11,13 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :lastname]) # Permit username during sign up
       devise_parameter_sanitizer.permit(:account_update, keys: [:username, :firstname, :lastname]) # Permit username during profile update
     end
+
+
+    private
+
+  def turbo_frame_request_variant
+    request.variant = :turbo_frame if turbo_frame_request?
+  end
+
+
 end
